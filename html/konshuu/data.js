4fde62f28dad22903834dce9bbdc123cf102ed41
Jie Luo <jieluo@berkeley.edu>
2019-04-13 16:28:58 -0700

    update hack job to include 9th issue of konshuu

--- html/konshuu/data.js
+++ html/konshuu/data.js
@@ -1,7 +1,7 @@
 // HACK JOB: clean up later
 
 var pdf_start = 42;
-var pdfs_limits = [13, 12, 8, 6, 12, 11, 13, 8];
+var pdfs_limits = [13, 12, 8, 6, 12, 11, 13, 9];
 
 var available_pdfs = [];
 var display_pdfs = [];
